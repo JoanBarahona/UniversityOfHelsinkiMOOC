8beff4ab8abbc7e0b3fe18096cc6b2fad2025151
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-26 23:44:06 +0200

    23 completed

--- part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,18 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give the first number:");
+        int n1 = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int n2 = Integer.valueOf(scanner.nextLine());
+        
+        int ans = n1 + n2;
+        
+        System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+        System.out.println(n1 + " - " + n2 + " = " + (n1 - n2));
+        System.out.println(n1 + " * " + n2 + " = " + (n1 * n2));
+        System.out.println(n1 + " / " + n2 + " = " + (1.0 * n1 / n2));
 
     }
 }
