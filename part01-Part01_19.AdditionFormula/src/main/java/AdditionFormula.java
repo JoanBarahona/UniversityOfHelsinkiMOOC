17948a3e2ab22317ae86a98f9de08503b3843151
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-26 23:01:50 +0200

    19 completed

--- part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
+++ part01-Part01_19.AdditionFormula/src/main/java/AdditionFormula.java
@@ -7,6 +7,14 @@ public class AdditionFormula {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        System.out.println("Give the first number:");
+        int firstValue = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondValue = Integer.valueOf(scanner.nextLine());
+        
+        //int result = firstValue + secondValue;
+        //System.out.println(result);
+        System.out.println(firstValue + " + " + secondValue + " = " + (firstValue + secondValue));
 
     }
 }
