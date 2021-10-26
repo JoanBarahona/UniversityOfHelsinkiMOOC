55199445c5019a2d684954096baad6566ecc0600
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-26 10:57:19 +0200

    5 completed

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -8,6 +8,12 @@ public class Message {
 
         System.out.println("Write a message:");
         // Write your program here
+        //Read the string written by the user, and assign it
+        //to program memory "String message = (string that was given as input)"
+        String message = scanner.nextLine();
+        
+        //Print the message written by the user
+        System.out.println(message);
 
     }
 }
