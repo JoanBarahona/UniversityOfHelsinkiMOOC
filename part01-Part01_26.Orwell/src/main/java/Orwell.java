1c566697804ab6e2e586bd130987168fba2abbcf
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 16:22:37 +0200

    26 completed == !=

--- part01-Part01_26.Orwell/src/main/java/Orwell.java
+++ part01-Part01_26.Orwell/src/main/java/Orwell.java
@@ -7,5 +7,16 @@ public class Orwell {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here
+        
+        System.out.println ("Give a number:");
+        int number = scan.nextInt();
+        
+        if (number != 1984) {
+            System.out.println("The number is not equal to 1984");
+        }
+        
+        if (number == 1984) {
+            System.out.println("Orwell");
+        }
     }
 }
