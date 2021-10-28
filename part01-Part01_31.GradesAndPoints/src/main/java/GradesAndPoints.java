d6beb818e8482272a4b8a0a707ac4923b0c10685
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 21:22:27 +0200

    31 completed

--- part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
+++ part01-Part01_31.GradesAndPoints/src/main/java/GradesAndPoints.java
@@ -5,6 +5,27 @@ public class GradesAndPoints {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give points [0-100]:");
+        int numberOne =scan.nextInt();
+        
+        if (numberOne < 0) {
+            System.out.println("Grade: ¡impossible");
+        } else if (numberOne <= 49){
+            System.out.println("Grade: failed");
+        } else if (numberOne <= 59) {
+            System.out.println("Grade: 1");
+        } else if (numberOne <= 69) {
+            System.out.println("Grade: 2");
+        } else if (numberOne <= 79) {
+            System.out.println("Grade: 3");
+        } else if (numberOne <= 89) {
+            System.out.println("Grade: 4");
+        } else if (numberOne <= 100){
+            System.out.println("Grade: 5");
+        } else {
+            System.out.println("Grade: ¡incredible!");
+        }
 
     }
 }
