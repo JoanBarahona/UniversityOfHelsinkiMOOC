c74b3b8fd938063f4223ff21f82cda8c4885a2fc
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 17:17:39 +0200

    30 completed else if

--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,19 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
+        int firstNumber = scan.nextInt();
+        System.out.println("Give the second number:");
+        int secondNumber = scan.nextInt();
+        
+        if (firstNumber < secondNumber){
+            System.out.println("Greater number is: " + secondNumber);
+        } else if (firstNumber == secondNumber) {
+            System.out.println("The numbers are equal!");
+        } else {
+            System.out.println("Greater number is: " + firstNumber);
+        }
 
     }
 }
