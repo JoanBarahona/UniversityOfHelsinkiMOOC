40bfab28a9599b9c8510d28980d512144bdcb9e0
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 23:30:48 +0200

    34 completed

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -4,8 +4,19 @@ import java.util.Scanner;
 public class Same {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
 
         // Write your program here. 
+        System.out.println("Enter the first string:");
+        String input1 = reader.nextLine();
+        
+        System.out.println("Enter the second string:");
+        String input2 = reader.nextLine();
+        
+        if (input1.equals(input2)){
+            System.out.println("Same");
+        } else {
+            System.out.println("Different");
+        }
     }
 }
