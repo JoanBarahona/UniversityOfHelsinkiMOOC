7557916c0dec35a61ab2bc1f01f5ee4c5a3264f3
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 15:20:04 +0200

    24 completed

--- part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -5,7 +5,16 @@ public class SpeedingTicket {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give speed:");
+        int number = scanner.nextInt();
+        
+        if(number > 120) {
+            System.out.println("Speeding ticket!");
+        }
+        
 
-        // Write your program here. 
+        
     }
+    
 }
