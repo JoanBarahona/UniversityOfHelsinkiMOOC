28cb8f33d86ab588ac0e7f4bff54065190ed9688
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 23:22:10 +0200

    33 completed string

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 public class Password {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
 
         // Write your program here 
+        System.out.println("Password?");
+        String input = reader.nextLine();
+        
+        if (input.equals("Caput Draconis")){
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
