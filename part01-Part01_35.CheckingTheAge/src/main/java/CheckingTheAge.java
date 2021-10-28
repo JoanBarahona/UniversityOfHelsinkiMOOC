f83269f6782e936fb2ca53b1c860f2c33f4bc594
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-28 23:50:45 +0200

    35 completed &&

--- part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
+++ part01-Part01_35.CheckingTheAge/src/main/java/CheckingTheAge.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 public class CheckingTheAge {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("How old are you? ");
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        if (number >= 0 && number <= 120) {
+            System.out.println("ok");
+        } else {
+            System.out.println("Impossible!");
+        }
 
     }
 }
