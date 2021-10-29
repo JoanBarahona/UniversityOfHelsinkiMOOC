b4bbe177acc7033e27221beff8784c1d06e9c9d4
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-29 21:13:37 +0200

    6 completed while

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,14 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Give a number");
+            int command = Integer.valueOf(scanner.nextLine());
+            if (command == 4 ) {
+                break;
+            }
+        }
 
     }
 }
