188f4760d504340e0f312b7bd9f7a011ed0ab520
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-29 15:41:37 +0200

    4 completed

--- part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
+++ part02-Part02_04.ComparingNumbers/src/main/java/ComparingNumbers.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 public class ComparingNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        
+        int numberOne = Integer.valueOf(reader.nextLine());
+        int numberTwo = Integer.valueOf(reader.nextLine());
+        
+        if (numberOne > numberTwo){
+            System.out.println(numberOne + " is greater than " + numberTwo + ".");
+        } else if (numberOne < numberTwo){
+            System.out.println(numberOne + " is smaller than " + numberTwo + ".");
+        } else {
+            System.out.println(numberOne + " is equal to " + numberTwo + ".");
+        }
 
     }
 }
