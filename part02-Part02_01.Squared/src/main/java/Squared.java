2fda8ee03165e4dc2ece23a95828150418eeea66
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-29 15:01:45 +0200

    1 completed

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 public class Squared {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        
+        //Declaring the variables and assigning user input to them
+        int number = Integer.valueOf(reader.nextLine());
+        
+        // Printing the result of the operation
+        System.out.println(number * number);
 
     }
 }
