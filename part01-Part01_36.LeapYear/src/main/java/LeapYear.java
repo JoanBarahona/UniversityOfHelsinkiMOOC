5432d50b25cb174d0a8e85f925a951a07d47ba71
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-29 09:26:54 +0200

    36 completed && || !=

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,15 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give a year:");
+        int number = scan.nextInt();
+        
+        if(((number % 4 == 0) && (number % 100 != 0)) || (number % 400 == 0)){
+            System.out.println("The year is a leap year");
+        } else {
+            System.out.println("The year is not a leap year");
+        }
 
     }
 }
