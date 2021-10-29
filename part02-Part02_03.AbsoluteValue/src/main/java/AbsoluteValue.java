a3eecac582ea627f4a9e758cc0ad79728d07502b
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-29 15:26:34 +0200

    3 completed

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 public class AbsoluteValue {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        
+        //Declaring the variables and assigning user input to them
+        int number = Integer.valueOf(reader.nextLine());
+        
+        
+        if (number < 0) {
+            System.out.println( number * -1);
+        } else {
+            System.out.println(number);
+        }
 
     }
 }
