0735d49edd350cc06a6c69d21784dc62cc54c1fb
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-30 12:52:52 +0200

    9 completed

--- part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -5,6 +5,20 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count = 0, input;
+        
+        do {
+            System.out.println("Give a number:");
+            input = scanner.nextInt();
+            
+            if (input <= 0) {
+                count++;
+            }
+           
+        } while (input != 0);
+        
+        System.out.println("Number of negative numbers: " + (count-1));
 
     }
 }
