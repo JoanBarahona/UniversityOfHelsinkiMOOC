6deb127621b3260bfbc7771030efc6988006adbd
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-10-30 13:11:57 +0200

    10 completed +

--- part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,21 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int ones = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            ones = ones + number;
+        }
+        
+        System.out.println("Sum of the numbers: " + ones);
 
     }
 }
