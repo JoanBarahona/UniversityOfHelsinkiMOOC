d7d20a7235d71854a0e874e9186e1b3ca73f6272
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-02 15:37:41 +0100

    14 start end

--- part02-Part02_14.Counting/src/main/java/Counting.java
+++ part02-Part02_14.Counting/src/main/java/Counting.java
@@ -5,6 +5,13 @@ public class Counting {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int userInput = Integer.valueOf(scanner.nextLine());
+        int count = 0;
+        
+        for (int i = 0; i <= userInput; i++) {
+            System.out.println(i);
+        }
 
     }
 }
