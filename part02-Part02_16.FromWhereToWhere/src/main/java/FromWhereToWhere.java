fdb6cd5baa62165700d10f720492122f12058fb5
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-02 16:17:47 +0100

    16 to from

--- part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ part02-Part02_16.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -7,5 +7,21 @@ public class FromWhereToWhere {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("where to?");
+        int endInput = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("where from?");
+        int startInput = Integer.valueOf(scanner.nextLine());
+        
+       /* while (startInput <= endInput) {
+            System.out.println(startInput);
+            startInput++;
+        }*/
+       
+        for (int i = startInput; i <= endInput; i++) {
+            if (endInput >= startInput) {
+                System.out.println(i);
+            }
+        }
     }
 }
