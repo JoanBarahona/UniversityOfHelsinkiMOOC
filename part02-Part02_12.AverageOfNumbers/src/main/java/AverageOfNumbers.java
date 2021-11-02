1e1310529a088cd412b0aafcc40fe5590d9b7a06
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-02 13:38:46 +0100

    12 average

--- part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,27 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        
+        int result = 0;
+        int count = 0;
+        double average = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int userInput = Integer.valueOf(scanner.nextLine());
+            
+            if (userInput == 0) {
+                break;
+            }//end if
+            
+            if (userInput != 0) {
+                count ++;
+                result += userInput;
+            }//end if
+            
+        }//end while
+        System.out.println("Average of the numbers: " + 1.0 * result / count);
 
-    }
+    }//end of main method
 }
