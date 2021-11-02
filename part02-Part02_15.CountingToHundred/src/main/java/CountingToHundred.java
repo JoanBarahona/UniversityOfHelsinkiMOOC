9dd298244f2fc2cbde3a95e85d90ef33f6501343
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-02 15:47:51 +0100

    15

--- part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,13 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int userInput = Integer.valueOf(scanner.nextLine());
+        
+        while (userInput <= 100) {
+            System.out.println(userInput);
+            userInput++;
+        }//end of while
 
-    }
+    }//end of mine method
 }
