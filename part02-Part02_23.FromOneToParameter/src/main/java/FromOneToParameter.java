9a6f245c98a0f181868822a34080dd94b80796f8
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-03 13:00:45 +0100

    23 completed

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -3,7 +3,19 @@
 public class FromOneToParameter {
 
     public static void main(String[] args) {
+        
+        printUntilNumber(2);
 
+    }
+    
+    public static void printUntilNumber(int userInput) {
+        
+        int counter = 1;
+        
+        while(counter <= userInput) {
+            System.out.println(counter);
+            counter++;
+        }
     }
 
 }
