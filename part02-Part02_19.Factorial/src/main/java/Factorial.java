aa32f03bc640f26980e3b0d56b057a8aa1f16891
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-03 09:21:54 +0100

    19 completed

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,19 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int sum = 1;
+        
+        System.out.println("Give a number:");
+        int numberInput = scanner.nextInt();
+        
+        for (int i = 1; i <= numberInput; i++) {
+            
+            sum *= i;
+            System.out.println(sum);
+        }
+        
+        System.out.println("Factorial: " + sum);
 
     }
 }
