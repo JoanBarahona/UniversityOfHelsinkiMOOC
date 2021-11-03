a32c45580514017375892fcf136111e224b50d15
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-03 13:46:14 +0100

    26 completed

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,20 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        divisibleByThreeInRange(3, 6);
+        //divisibleByThreeInRange(2,10);
 
+    }
+    
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        while (beginning <= end) {
+            if (beginning %3 == 0) {
+                System.out.println(beginning);
+            }
+            
+            beginning++;
+        }
     }
 
 }
