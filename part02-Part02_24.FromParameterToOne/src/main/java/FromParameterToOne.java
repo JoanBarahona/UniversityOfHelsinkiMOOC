15afbe5f58066db1672be8f9050bbe2d5c5661ac
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-03 13:11:13 +0100

    24 completed

--- part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
+++ part02-Part02_24.FromParameterToOne/src/main/java/FromParameterToOne.java
@@ -3,8 +3,19 @@
 public class FromParameterToOne {
 
     public static void main(String[] args) {
+        
+        printFromNumberToOne(5);
 
-
+    }
+    
+    public static void printFromNumberToOne(int input) {
+        
+        
+        
+        while(input > 0) {
+            System.out.println(input);
+            input--;
+        }
     }
 
 }
