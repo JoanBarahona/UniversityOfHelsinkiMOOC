2083c1d84b8e3bc0b4a476896f899740732b6cfe
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-03 13:21:16 +0100

    25 completed

--- part02-Part02_25.Division/src/main/java/Division.java
+++ part02-Part02_25.Division/src/main/java/Division.java
@@ -8,8 +8,15 @@ public class Division {
         // try it out here. Fopr example division(3,5);
         // should print "0.6"
 
-        // division(3, 5);
+        division(3, 5);
     }
 
     // implement the method here
+    public static void division (int numerator, int denominator){
+       
+        double result = 1.0 * numerator / denominator;
+        
+        System.out.println(result);
+        
+    }
 }
