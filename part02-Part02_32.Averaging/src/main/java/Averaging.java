3ca0a3c376073c2bca9e22e55471eff82bd43a47
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-04 12:38:07 +0100

    32 completed

--- part02-Part02_32.Averaging/src/main/java/Averaging.java
+++ part02-Part02_32.Averaging/src/main/java/Averaging.java
@@ -4,12 +4,14 @@ public class Averaging {
     // implement the sum method here again
     public static int sum(int number1, int number2, int number3, int number4) {
         // write some code here
-        return 0;
+        int sum = number1 + number2 + number3 + number4;
+        return sum;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
         //write some code here
-        return -1;
+        double average = 1.0 * sum(number1, number2, number3, number4) / 4;
+        return average;
     }
 
     public static void main(String[] args) {
