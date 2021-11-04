7c786049e1b3d06722009bf1ce296cbabcae5668
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-04 10:51:14 +0100

    27 completed

--- part02-Part02_27.NumberUno/src/main/java/NumberUno.java
+++ part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -5,7 +5,13 @@ public class NumberUno {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+       
 
     }
+    
+    public static int numberUno() {
+        return 1;
+    } 
 
 }
