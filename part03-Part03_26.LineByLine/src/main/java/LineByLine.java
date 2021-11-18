26d92ed55a98048868f0935eea08a5aa84c07162
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-18 13:52:40 +0100

    26 completed

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -2,23 +2,22 @@
 import java.util.Scanner;
 
 public class LineByLine {
-
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] arrayWords;
-        
-        while (true){
-            String userInput = scanner.nextLine();
-            if (userInput.equals("")){
-                break;
-            }
-            
-            arrayWords = userInput.split(" ");
-            
-            for (String word : arrayWords) {
-                System.out.println(word);
-            }
-        }
-
+      Scanner scanner = new Scanner(System.in);
+      String[] arrayWords;
+      
+      while (true) {
+          String userInput = scanner.nextLine();
+          if (userInput.equals("")){
+              break;
+          }
+          
+          arrayWords = userInput.split(" ");
+          
+          for (String word : arrayWords) {
+              System.out.println(word);
+          }
+      }
     }
 }
