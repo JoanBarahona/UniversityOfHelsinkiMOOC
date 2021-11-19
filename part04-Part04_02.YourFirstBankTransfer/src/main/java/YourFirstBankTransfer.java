2a0180908273b80f55c78afb1d2e0e22f440b277
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-19 14:04:47 +0100

    2 completed

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,14 +4,14 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
-        Account a1 = new Account ("Matthews account", 1000);
-        Account a2 = new Account ("My account", 0);
+        Account joanAccount = new Account ("Matthews account", 1000);
+        Account myAccount = new Account ("My account", 0);
         
-        a1.withdrawal(100);
-        a2.deposit(100);
+        joanAccount.withdrawal(100);
+        myAccount.deposit(100);
         
-        System.out.println(a1);
-        System.out.println(a2);
+        System.out.println(joanAccount);
+        System.out.println(myAccount);
         
     }
 }
