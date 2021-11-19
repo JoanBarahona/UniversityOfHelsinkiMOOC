45a0decc23fe10c9fefee74ff9a8492f5a312ba8
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-19 13:56:35 +0100

    1 completed

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,9 +4,9 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
-        Account Joan = new Account("Joan", 100);
+        Account joanAccount = new Account("Joan", 100);
         
-        Joan.deposit(20.00);
-        System.out.println(Joan);
+        joanAccount.deposit(20.00);
+        System.out.println(joanAccount);
     }
 }
