439b2c64e9a02347e4891bc385a9e798f9710a79
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-23 13:26:37 +0100

    15 completed

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,6 +5,32 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        Statistics statistics = new Statistics();
+        Statistics statisticsEven = new Statistics();
+        Statistics statisticsOdd = new Statistics();
+        
+        System.out.println("Enter numbers: ");
+        
+        while (true){
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == -1){
+                break;
+            }
+            
+            if(number % 2 == 0){
+                statisticsEven.addNumber(number);
+            } else {
+                statisticsOdd.addNumber(number);
+            }
+            
+            statistics.addNumber(number);
+        }
+        
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + statisticsEven.sum());
+        System.out.println("Sum of odd numbers: " + statisticsOdd.sum());
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,38 @@
 
 public class Statistics {
+    
+    private int count;
+    private int sum;
+    
+    public Statistics(){
+        
+        this.count = 0;
+        this.sum = 0;
+    }
+    
+    public void addNumber(int number){
+        
+        this.count++;
+        this.sum += number;
+    }
+    
+    public int getCount(){
+        
+        return this.count;
+    }
+    
+    public int sum(){
+        
+        return this.sum;
+    }
+    
+    public double average(){
+        double result = 0;
+        
+        if(this.sum != 0 && this.count != 0){
+            
+            result = 1.0 * this.sum / this.count;
+        }
+        return result;
+    }
 }
