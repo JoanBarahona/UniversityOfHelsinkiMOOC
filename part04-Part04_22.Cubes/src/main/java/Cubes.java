e459ca30da49ab7f7605b8bbcebe465acd1145d7
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-25 12:55:18 +0100

    22 completed

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,21 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+                     
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")){
+                break;
+            }
+            
+            int inputToInt = Integer.valueOf(input);
+            
+            int result = (inputToInt * inputToInt) * inputToInt;
+            
+            System.out.println(result);
+        }
+        
+        
     }
 }
