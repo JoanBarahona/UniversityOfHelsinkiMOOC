fa158b3ab8427d8424a23a1ad4822e934019be30
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-25 12:39:19 +0100

    21 completed

--- part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,20 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int count = 0;
+        
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.equalsIgnoreCase("end")){
+                break;
+            }
+            
+            count++;
+        }
+        
+        System.out.println(count);
 
     }
 }
