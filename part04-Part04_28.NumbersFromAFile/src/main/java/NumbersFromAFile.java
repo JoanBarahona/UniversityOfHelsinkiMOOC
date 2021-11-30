80674a2ad23a153f3878e34239608dcd7ea17ba8
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-11-30 16:10:47 +0100

    28 completed

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -13,6 +13,23 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        
+        int totalNumbers = 0;
+        
+        try(Scanner fileReader = new Scanner(Paths.get(file))){
+            while(fileReader.hasNextLine()){
+                int number = Integer.valueOf(fileReader.nextLine());
+                
+                if(number >= lowerBound && number <= upperBound){
+                    totalNumbers++;
+                }
+                
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+        System.out.println("Numbers: " + totalNumbers);
 
     }
 
