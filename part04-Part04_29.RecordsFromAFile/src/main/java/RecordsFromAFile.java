67f43987a3adb49bec461aaa051a478993df6fdc
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-12-01 12:18:50 +0100

    29 completed

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,36 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+        
+        try(Scanner fileScanner = new Scanner(Paths.get(file))){
+            
+            while(fileScanner.hasNextLine()){
+                String[]splitString = fileScanner.nextLine().split(",");
+                
+                if(splitString.equals("")){
+                    continue;
+                }
+                
+                String name = splitString[0];
+                
+                int age = Integer.valueOf(splitString[1]);
+                
+                if(age > 1 || age == 0){
+                    
+                    System.out.println (name + ", age: " + age + " years");
+                    
+                }else{
+                    
+                    System.out.println(name +", age: " + age +" year");
+                }
+            }
+        }catch(Exception e){
+            
+            System.out.println("File was not able to be read" + e.getMessage());
+        }
 
     }
 }
