1f74e5f59b431be1993cc62c23dcfa3180ed2d9e
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2021-12-03 12:28:59 +0100

    1 completed

--- part05-Part05_01.OneMinute/src/main/java/Program.java
+++ part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -5,6 +5,22 @@ public class Program {
 
     public static void main(String[] args) {
         // You can test your program here
+        Timer timer = new Timer();
+        
+        while(true){
+            
+            System.out.println(timer);
+            timer.advance();
+            
+            try{
+                
+                Thread.sleep(3);
+            
+            }catch(Exception e){
+                
+            }
+            
+        }
 
 
     }
