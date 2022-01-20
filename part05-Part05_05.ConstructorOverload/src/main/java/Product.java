48d0d8aa599dcfea9fc4225e10074860a48b5735
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2022-01-20 13:53:33 +0100

    5 completed

--- part05-Part05_05.ConstructorOverload/src/main/java/Product.java
+++ part05-Part05_05.ConstructorOverload/src/main/java/Product.java
@@ -10,8 +10,19 @@ public class Product {
         this.location = location;
         this.weight = weight;
     }
-
     
+    public Product(String name){
+        this(name, "shelf", 1);
+    }
+    
+    public Product(String name, String location){
+        this(name, location, 1);
+    }
+    
+    public Product(String name, int weight){
+        this(name, "shelf", weight);
+    }
+
     public String getName() {
         return name;
     }
