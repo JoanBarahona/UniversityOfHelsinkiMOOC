4240b84709b5db2710b21556b243459ca794297c
JoanBarahona <82812582+JoanBarahona@users.noreply.github.com>
2022-01-28 14:07:54 +0100

    9 completed

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,24 @@
 
 public class HealthStation {
+private int weighingsPerformed;
 
+    public HealthStation(){
+        this.weighingsPerformed=0;
+    }
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        
+        this.weighingsPerformed++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person){
+        person.setWeight(person.getWeight()+1);
+    }
+    
+    public int weighings(){
+        return this.weighingsPerformed;
     }
 
 }
